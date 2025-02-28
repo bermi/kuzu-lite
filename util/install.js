a9f8a2da51b695ab0068874c12cfe77f00dfe67d
SeanLi <sean@kineviz.com>
2025-02-28 09:56:13 +0800

    reduce the npm package size

--- util/install.js
+++ util/install.js
@@ -1,36 +1,58 @@
-const os = require("os");
-const childProcess = require("child_process");
 const path = require("path");
-const fs = require("fs");
-const fsCallback = require("fs");
 const process = require("process");
+const fs = require("fs");
+const https = require("https");
+const { HttpsProxyAgent } = require('https-proxy-agent');
 
 const platform = process.platform;
 const arch = process.arch;
 const rootDir = path.join(__dirname, "..");
 
-const isAlpine = fs.readFileSync('/etc/os-release', 'utf8').includes('Alpine Linux');
+const isAlpine = platform == "linux" && fs.readFileSync('/etc/os-release', 'utf8').includes('Alpine Linux');
 if (isAlpine) {
-  platform = "alpine"
+  platform = "alpine";
 }
 
-const prebuiltPath = path.join(
-  rootDir,
-  "prebuilt",
-  `kuzujs-${platform}-${arch}.node`
-);
+const baseURL = "https://raw.githubusercontent.com/Kineviz/kuzu-lite/refs/heads/master/prebuilt";
+const prebuiltURL = `${baseURL}/kuzujs-${platform}-${arch}.node`;
 
-if (fsCallback.existsSync(prebuiltPath)) {
-  console.log("Prebuilt binary is available.");
-  console.log("Copying prebuilt binary to package directory...");
-  fs.copyFileSync(prebuiltPath, path.join(rootDir, "kuzujs.node"));
-  console.log(
-    `Copied ${prebuiltPath} -> ${path.join(rootDir, "kuzujs.node")}.`
-  );
- 
-  console.log("Done!");
-  process.exit(0);
-} else {
-  console.log("Prebuilt binary is not available, building from source...");
-}
+console.log(`Downloading prebuilt binary from ${prebuiltURL}...`);
 
+const targetPath = path.join(rootDir, "kuzujs.node");
+
+const download = (url, dest) => {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(dest);
+    const proxy = process.env.HTTP_PROXY;
+    const agent = proxy ? new HttpsProxyAgent(proxy) : undefined;
+
+    const reqOptions = { agent };
+
+    https.get(url, reqOptions, (response) => {
+      if (response.statusCode !== 200) {
+        reject(new Error(`Failed to download: ${response.statusCode} ${response.statusMessage}`));
+        return;
+      }
+
+      response.pipe(file);
+
+      file.on('finish', () => {
+        file.close(() => resolve());
+      });
+    }).on('error', (err) => {
+      fs.unlink(dest, () => {});
+      reject(err);
+    });
+  });
+};
+
+download(prebuiltURL, targetPath)
+  .then(() => {
+    console.log(`Successfully downloaded to ${targetPath}`);
+    console.log("Done!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(`Error downloading prebuilt binary: ${err.message}`);
+    console.log("Prebuilt binary download failed, building from source...");
+  });
